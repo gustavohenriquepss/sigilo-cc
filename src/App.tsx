8ff5f55dd32054b9a0bf7947ce5bad93d2db5266
senaarth <senaarth@gmail.com>
2025-06-27 17:07:10 -0300

    analytics

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import {Analytics} from "@vercel/analytics/react"
 import Index from "./pages/Index";
 import Support from "./pages/Support";
 import NotFound from "./pages/NotFound";
@@ -26,6 +27,7 @@ const AppContent = () => {
           <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
+      <Analytics />
     </>
   );
 };
